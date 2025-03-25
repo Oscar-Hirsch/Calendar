8dd83b9d239cc105ac3c422d961b44ae7ceb056c
Oscar-Hirsch <oscar@completeaudio.com>
2025-03-25 16:32:04 +0100

    Calendar create in main without functions

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         String [] monthsArray = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
-        for (int i = 1; i <= 12; i++) {
-            System.out.println(monthsArray[i - 1]);
-            month(i);
+        String weekDays = "Mo Di Mi Do Fr Sa So";
+        int [] daysofMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int startingDay = 1;
 
-        }
-
-    }
-
-    public static void month(int month) {
-        String [] weekdays = {"Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"};
-        for (int i = 1; i <= 7; i++) {
-            System.out.print(weekdays[i - 1]+ " ");
-        }
-
-        if (month == 2) {
-            for (int i = 1; i <= 28;) {
-                System.out.println("");
-                for (int days = 1; days <= 7; days++) {
-                    System.out.print(i + " ");
-                    i++;
+        for (int i = 0; i < monthsArray.length; i++) {
+            System.out.println(monthsArray[i]);
+            System.out.println(weekDays);
+            for (int j = 1; j <= daysofMonth[i]; j++) {
+                System.out.print(j + " ");
+                if ((j + startingDay) % 7 == 0) {
+                    System.out.println();
                 }
             }
-        } else if (isEven(month)) {
-            for (int i = 1; i <= 30; i++) {
-                System.out.println(i);
-            }
-        } else {
-            for (int i = 1; i <= 31; i++) {
-                System.out.println(i);
-            }
-
+            System.out.println();
         }
+
     }
 
-    public static boolean isEven(int number) {
-        return number % 2 == 0;
-    }
+
 }
